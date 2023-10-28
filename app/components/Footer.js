028c06d605b377acce49a2bc2192a1a3a8faf28a
Olyadtemesgen <olexyit@gmail.com>
2023-10-28 16:35:20 +0300

    resolve eslint errors

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import {
   FaFacebookF,
@@ -14,7 +15,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row gap-8 xl:justify-around">
           {/* Column 1 */}
           <div>
-            <img src="/bellx-logo.svg" alt="Company Logo" className="w-24" />
+            <Image src="/bellx-logo.svg" alt="Company Logo" className="w-24" />
             <p className="text-lg mb-3">BellX Software Solutions</p>
             <p className="text-sm">
               Your Agile Tech Partner That Helps You Accelerate Growth
